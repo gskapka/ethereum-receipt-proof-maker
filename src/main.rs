bf6f2b3ccc0e487bc7b227d86e07f8e5032fe11d
Greg Kapka <greg@oraclize.it>
2019-07-27 22:25:01 +0100

    feat(main): adds nested json http call & json response parsing

---
+++ src/main.rs
@@ -0,0 +1,47 @@
+extern crate reqwest;
+
+#[macro_use]
+extern crate serde_json;
+use serde::{Deserialize, Serialize};
+
+fn get_json(block_hash: String) -> serde_json::Value {
+    json!({
+        "id": "1",
+        "jsonrpc": "2.0",
+        "method": "eth_getBlockByHash",
+        "params": [ block_hash, true ],
+    })
+}
+
+#[derive(Debug, Serialize, Deserialize)]
+struct Response {
+    result: Block
+}
+
+#[derive(Debug, Serialize, Deserialize)]
+struct Block {
+    transactions: Vec<Transaction>
+}
+
+#[allow(non_snake_case)]
+#[derive(Debug, Serialize, Deserialize)]
+struct Transaction {
+    blockHash: String
+}
+
+fn main() {
+
+    let client = reqwest::Client::new();
+
+    let block_hash = "0x1ddd540f36ea0ed23e732c1709a46c31ba047b98f1d99e623f1644154311fe10";
+
+    let mut res = client.post("https://rpc.slock.it/mainnet")
+        .json(&get_json(block_hash.to_string()))
+        .send()
+        .unwrap();
+
+    let result = res.text().unwrap();
+
+    let r: Response = serde_json::from_str(&result).unwrap();
+    println!("{:?}", r.result.transactions)
+}
