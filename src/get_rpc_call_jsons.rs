6143e1ba724d5bef15f42370f5c6f58e51aec92b
Greg Kapka <greg@oraclize.it>
2019-07-28 17:03:46 +0100

    feat(rpc-jsons): adds getter for rpc calls jsons (& tests)

---
+++ src/get_rpc_call_jsons.rs
@@ -0,0 +1,59 @@
+use std::result;
+use serde_json::Value;
+use crate::errors::AppError;
+
+type Result<T> = result::Result<T, AppError>;
+
+pub fn get_block_by_block_hash_json(block_hash: String) -> Result<Value> {
+    Ok(
+        json!({
+            "id": "1",
+            "jsonrpc": "2.0",
+            "method": "eth_getBlockByHash",
+            "params": [ block_hash, false ],
+        })
+    )
+}
+
+pub fn get_transaction_receipt_json(tx_hash: String) -> Result<Value> {
+    Ok(
+        json!({
+            "id": "1",
+            "jsonrpc": "2.0",
+            "method": "eth_getTransactionReceipt",
+            "params": [ tx_hash ],
+        })
+    )
+}
+
+#[cfg(test)]
+#[allow(unused_doc_comments)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn should_get_block_by_block_hash_json_correctly() {
+        let dummy_hash = "0xc0ffee".to_string();
+        let expected_result = format!("\"{}\"", dummy_hash.clone());
+        let result = get_block_by_block_hash_json(dummy_hash)
+            .unwrap();
+        assert!("\"1\"" == result["id"].to_string());
+        assert!("\"2.0\"" == result["jsonrpc"].to_string());
+        assert!("\"eth_getBlockByHash\"" == result["method"].to_string());
+        assert!("false" == result["params"][1].to_string());
+        assert!(expected_result == result["params"][0].to_string());
+    }
+
+    #[test]
+    fn should_get_transaction_receipt_json_correctly() {
+        let dummy_hash = "0xc0ffee".to_string();
+        let expected_result = format!("\"{}\"", dummy_hash.clone());
+        let result = get_transaction_receipt_json(dummy_hash)
+            .unwrap();
+        assert!("\"1\"" == result["id"].to_string());
+        assert!("\"2.0\"" == result["jsonrpc"].to_string());
+        assert!("\"eth_getTransactionReceipt\"" == result["method"].to_string());
+        assert!(expected_result == result["params"][0].to_string());
+    }
+}
+
