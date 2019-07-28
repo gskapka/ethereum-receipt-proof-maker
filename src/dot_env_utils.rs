f843162b0168f148998778145ca420e56d615237
Greg Kapka <greg@oraclize.it>
2019-07-28 22:58:45 +0100

    feat(dot-env): adds restore file method & tests

--- src/dot_env_utils.rs
+++ src/dot_env_utils.rs
@@ -17,14 +17,15 @@ pub fn delete_env_file() -> Result<()> {
     Ok(fs::remove_file(&DOT_ENV_PATH)?)
 }
 
+pub fn restore_env_file(data: String) -> Result<()> {
+    Ok(fs::write(&DOT_ENV_PATH, data)?)
+}
+
 #[cfg(test)]
 mod tests {
     use super::*;
     use std::path::Path;
 
-    fn restore_original_config_file(data: String) -> Result<()> {
-        Ok(fs::write(&DOT_ENV_PATH, data)?)
-    }
 
     #[test]
     fn should_read_existing_env_file_correctly() {
@@ -36,32 +37,72 @@ mod tests {
 
     #[test]
     fn should_delete_env_file_correctly() {
-        let mut original_file: String = "none".to_string();
-        if (Path::new(&DOT_ENV_PATH).exists()) {
-            original_file = read_env_file().unwrap();
-            delete_env_file().unwrap();
-            assert!(!Path::new(&DOT_ENV_PATH).exists());
-            restore_original_config_file(original_file).unwrap();
-            assert!(Path::new(&DOT_ENV_PATH).exists());
-        } else {
-            write_env_file().unwrap();
-            assert!(Path::new(&DOT_ENV_PATH).exists());
-            delete_env_file().unwrap();
-            assert!(!Path::new(&DOT_ENV_PATH).exists());
+        match (Path::new(&DOT_ENV_PATH).exists()) {
+            true => {
+                let original_file = read_env_file().unwrap();
+                delete_env_file().unwrap();
+                assert!(!Path::new(&DOT_ENV_PATH).exists());
+                restore_env_file(original_file.clone()).unwrap();
+                assert!(Path::new(&DOT_ENV_PATH).exists());
+                let file = read_env_file().unwrap();
+                assert!(file == original_file);
+            },
+            false => {
+                write_env_file().unwrap();
+                assert!(Path::new(&DOT_ENV_PATH).exists());
+                delete_env_file().unwrap();
+                assert!(!Path::new(&DOT_ENV_PATH).exists());
+            }
         }
     }
 
     #[test]
     fn should_write_env_file_correctly() {
-        let mut original_file: String = "none".to_string();
-        if (Path::new(&DOT_ENV_PATH).exists()) {
-            original_file = read_env_file().unwrap();
-            delete_env_file().unwrap();
+        match (Path::new(&DOT_ENV_PATH).exists()) {
+            true => {
+                let original_file = read_env_file().unwrap();
+                delete_env_file().unwrap();
+                assert!(!Path::new(&DOT_ENV_PATH).exists());
+                write_env_file().unwrap();
+                assert!(Path::new(&DOT_ENV_PATH).exists());
+                delete_env_file().unwrap();
+                restore_env_file(original_file).unwrap();
+            },
+            false => {
+                write_env_file().unwrap();
+                assert!(Path::new(&DOT_ENV_PATH).exists());
+                delete_env_file().unwrap();
+                assert!(!Path::new(&DOT_ENV_PATH).exists());
+            }
         }
-        assert!(!Path::new(&DOT_ENV_PATH).exists());
-        write_env_file().unwrap();
-        assert!(Path::new(&DOT_ENV_PATH).exists());
-        delete_env_file().unwrap();
-        restore_original_config_file(original_file).unwrap();
+    }
+
+    #[test]
+    fn should_restore_env_file_correctly() {
+        match Path::new(&DOT_ENV_PATH).exists() {
+            true => {
+                let mut original_file = "none".to_string();
+                original_file = read_env_file().unwrap();
+                delete_env_file().unwrap();
+                assert!(!Path::new(&DOT_ENV_PATH).exists());
+                restore_env_file(original_file.clone());
+                assert!(Path::new(&DOT_ENV_PATH).exists());
+                let result = read_env_file().unwrap();
+                assert!(result == original_file)
+            },
+            false => {
+                write_env_file().unwrap();
+                assert!(Path::new(&DOT_ENV_PATH).exists());
+                let file = read_env_file().unwrap();
+                delete_env_file().unwrap();
+                assert!(!Path::new(&DOT_ENV_PATH).exists());
+                restore_env_file(file.clone()).unwrap();
+                assert!(Path::new(&DOT_ENV_PATH).exists());
+                let result = read_env_file().unwrap();
+                assert!(result == file);
+                delete_env_file().unwrap();
+                assert!(!Path::new(&DOT_ENV_PATH).exists());
+            }
+        }
     }
 }
