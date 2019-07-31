7d82963e1e86c5e9cdf4542af1b68e95c47b2266
Greg Kapka <greg@oraclize.it>
2019-07-31 11:07:38 +0100

    feat(state): make state setters fail on overwrites

--- src/state.rs
+++ src/state.rs
@@ -5,14 +5,19 @@ use crate::errors::AppError;
 
 type Result<T> = result::Result<T, AppError>;
 
+#[derive(Clone)]
 pub struct State {
     block: Option<Block>,
     tx_hash: Option<H256>,
     endpoint: Option<String>,
 }
 
-fn get_state_err_str(substring: &str) -> String {
+fn get_not_in_state_err(substring: &str) -> String {
     format!("✘ No {} in state!" , substring)
+}
+
+fn get_no_overwrite_state_err(substring: &str) -> String {
+    format!("✘ Cannot overwrite {} in state!" , substring)
 }
 
 impl State {
@@ -26,39 +31,54 @@ impl State {
         )
     }
 
-    pub fn add_block_to_state(mut self, block: Block) -> Result<State> {
-        self.block = Some(block);
-        Ok(self)
-    }
-
-    pub fn add_endpoint_to_state(mut self, endpoint: String) -> Result<State> {
-        self.endpoint= Some(endpoint);
-        Ok(self)
-    }
-
-    pub fn add_tx_hash_to_state(mut self, tx_hash: H256)-> Result<State> {
-        self.tx_hash= Some(tx_hash);
-        Ok(self)
-    }
-
-    pub fn get_block_from_state(self) -> Result<Block> {
+    pub fn set_block_in_state(mut self, block: Block) -> Result<State> {
         match self.block {
-            Some(block) => Ok(block),
-            _ => Err(AppError::Custom(get_state_err_str("block")))
+            Some(_) => Err(AppError::Custom(get_no_overwrite_state_err("block"))),
+            _ => {
+                self.block = Some(block);
+                Ok(self)
+            }
         }
     }
 
-    pub fn get_endpoint_from_state(self) -> Result<String> {
+    pub fn set_endpoint_in_state(mut self, endpoint: String) -> Result<State> {
+        match self.endpoint {
+            Some(_) => Err(AppError::Custom(get_no_overwrite_state_err("endpoint"))),
+            _ => {
+                self.endpoint = Some(endpoint);
+                Ok(self)
+            }
+        }
+    }
+
+    pub fn set_tx_hash_in_state(mut self, tx_hash: H256)-> Result<State> {
+        match self.tx_hash{
+            Some(_) => Err(AppError::Custom(get_no_overwrite_state_err("transaction hash"))),
+            _ => {
+                self.tx_hash= Some(tx_hash);
+                Ok(self)
+            }
+        }
+    }
+
+    pub fn get_block_from_state(self) -> Result<Block> { // TODO: Should these return a tuple of state + thing?
+        match self.block {
+            Some(block) => Ok(block),
+            _ => Err(AppError::Custom(get_not_in_state_err("block")))
+        }
+    }
+
+    pub fn get_endpoint_from_state(self) -> Result<String> { // TODO: Ibid. So as not to "consume" the state?
         match self.endpoint {
             Some(endpoint) => Ok(endpoint),
-            _ => Err(AppError::Custom(get_state_err_str("endpoint")))
+            _ => Err(AppError::Custom(get_not_in_state_err("endpoint")))
         }
     }
 
     pub fn get_tx_hash_from_state(self) -> Result<H256> {
         match self.tx_hash {
             Some(tx_hash) => Ok(tx_hash),
-            _ => Err(AppError::Custom(get_state_err_str("transaction hash")))
+            _ => Err(AppError::Custom(get_not_in_state_err("transaction hash")))
         }
     }
 }
@@ -68,13 +88,23 @@ mod tests {
     use super::*;
 
     #[test]
-    fn should_get_err_string() {
+    fn should_get_no_state_err_string() {
         let thing = "thing".to_string();
         let expected_result = "✘ No thing in state!";
-        let result = get_state_err_str(&thing);
+        let result = get_not_in_state_err(&thing);
         assert!(result == expected_result)
 
     }
+
+    #[test]
+    fn should_get_no_overwrite_err_string() {
+        let thing = "thing".to_string();
+        let expected_result = "✘ Cannot overwrite thing in state!";
+        let result = get_no_overwrite_state_err(&thing);
+        assert!(result == expected_result)
+
+    }
+
     #[test]
     fn should_get_empty_state_successfully() {
         let state = State::get_initial_state()
@@ -87,7 +117,7 @@ mod tests {
             .unwrap();
         match State::get_block_from_state(state) {
             Err(AppError::Custom(e)) => {
-                let expectedErr = get_state_err_str("block");
+                let expectedErr = get_not_in_state_err("block");
                 assert!(e == expectedErr);
             },
             Ok(_) => panic!("Block should not be initialised in state!"),
@@ -101,7 +131,7 @@ mod tests {
             .unwrap();
         match State::get_endpoint_from_state(state) {
             Err(AppError::Custom(e)) => {
-                let expectedErr = get_state_err_str("endpoint");
+                let expectedErr = get_not_in_state_err("endpoint");
                 assert!(e == expectedErr);
             },
             Ok(_) => panic!("Endpoint should not be initialised in state!"),
@@ -115,7 +145,7 @@ mod tests {
             .unwrap();
         match State::get_tx_hash_from_state(state) {
             Err(AppError::Custom(e)) => {
-                let expectedErr = get_state_err_str("transaction hash");
+                let expectedErr = get_not_in_state_err("transaction hash");
                 assert!(e == expectedErr);
             },
             Ok(_) => panic!("Endpoint should not be initialised in state!"),
@@ -129,7 +159,7 @@ mod tests {
         let expected_result = "expected endpoint".to_string();
         let state = State::get_initial_state()
             .unwrap();
-        let new_state = State::add_endpoint_to_state(state, expected_result.clone())
+        let new_state = State::set_endpoint_in_state(state, expected_result.clone())
             .unwrap();
         let result = State::get_endpoint_from_state(new_state)
             .unwrap();
@@ -138,11 +168,11 @@ mod tests {
     */
 
     #[test]
-    fn should_add_endpoint_to_state() {
+    fn should_set_endpoint_to_state() {
         let expected_result = "expected endpoint".to_string();
         let state = State::get_initial_state()
             .unwrap();
-        let new_state = State::add_endpoint_to_state(state, expected_result.clone())
+        let new_state = State::set_endpoint_in_state(state, expected_result.clone())
             .unwrap();
         let result = State::get_endpoint_from_state(new_state)
             .unwrap();
