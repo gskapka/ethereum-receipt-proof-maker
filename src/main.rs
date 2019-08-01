f304e2d360d4c5f6bb40d6d826eada9cefa381c8
Greg Kapka <greg@oraclize.it>
2019-08-01 16:56:36 +0100

    feat(main): <- start to build main pipeline in that

--- src/main.rs
+++ src/main.rs
@@ -1,100 +1,43 @@
+#![feature(try_trait)]
+#![feature(exclusive_range_pattern)]
+
+extern crate serde;
 extern crate reqwest;
 
-#[macro_use]
-extern crate serde_json;
-use serde::{Deserialize, Serialize};
+mod utils;
+mod state;
+mod types;
+mod errors;
+mod constants;
+mod usage_info;
+mod get_config;
+mod test_utils;
+mod get_endpoint;
+mod dot_env_utils;
+mod parse_cli_args;
+mod validate_tx_hash;
+mod validate_cli_args;
+mod get_rpc_call_jsons;
+mod initialize_state_from_cli_args;
 
-fn get_get_block_by_block_hash_json(block_hash: String) -> serde_json::Value {
-    json!({
-        "id": "1",
-        "jsonrpc": "2.0",
-        "method": "eth_getBlockByHash",
-        "params": [ block_hash, false ],
-    })
-}
+#[macro_use] extern crate serde_json;
+#[macro_use] extern crate serde_derive;
+#[macro_use] extern crate serial_test_derive;
 
-fn get_get_transaction_receipt_json(tx_hash: String) -> serde_json::Value {
-    json!({
-        "id": "1",
-        "jsonrpc": "2.0",
-        "method": "eth_getTransactionReceipt",
-        "params": [ tx_hash ],
-    })
-}
-
-#[derive(Debug, Serialize, Deserialize)]
-struct GetBlockResponse {
-    result: Block
-}
-
-#[derive(Debug, Serialize, Deserialize)]
-struct Block {
-    transactions: Vec<String>
-}
-
-#[derive(Debug, Serialize, Deserialize)]
-struct GetReceiptResponse {
-    result: Receipt
-}
-
-#[allow(non_snake_case)]
-#[derive(Debug, Serialize, Deserialize)]
-struct Receipt {
-    to: String,
-    from: String,
-    logs: Vec<Log>,
-    status: String,
-    gasUsed: String,
-    blockHash: String,
-    logsBloom: String,
-    blockNumber: String,
-    root: serde_json::Value, // because it's null in this case!
-    transactionHash: String,
-    transactionIndex: String,
-    cumulativeGasUsed: String,
-    contractAddress: serde_json::Value, // because it's null in this case!
-}
-
-#[allow(non_snake_case)]
-#[derive(Debug, Serialize, Deserialize)]
-struct Log {
-    data: String,
-    removed: bool,
-    r#type: String,
-    address: String,
-    logIndex: String,
-    blockHash: String,
-    blockNumber: String,
-    topics: Vec<String>,
-    transactionHash: String,
-    transactionIndex: String,
-}
+use crate::state::State;
+use crate::parse_cli_args::parse_cli_args;
+use crate::validate_cli_args::validate_cli_args;
+use crate::get_endpoint::get_endpoint_and_set_in_state;
+use crate::initialize_state_from_cli_args::initialize_state_from_cli_args;
 
 fn main() {
-    let client = reqwest::Client::new();
-
-    let block_hash = "0x1ddd540f36ea0ed23e732c1709a46c31ba047b98f1d99e623f1644154311fe10";
-
-    let mut res = client.post("https://rpc.slock.it/mainnet")
-        .json(&get_get_block_by_block_hash_json(block_hash.to_string()))
-        .send()
-        .unwrap();
-
-    let result = res.text().unwrap();
-
-    let r: GetBlockResponse = serde_json::from_str(&result).unwrap();
-    println!("{:?}", r.result.transactions);
-    println!("Length: {:?}", r.result.transactions.len());
-
-    let tx_hash = &r.result.transactions[0];
-    let mut res = client.post("https://rpc.slock.it/mainnet")
-        .json(&get_get_transaction_receipt_json(tx_hash.to_string()))
-        .send()
-        .unwrap();
-
-    let result = res.text().unwrap();
-    println!("{:?}", result);
-    let rec: GetReceiptResponse = serde_json::from_str(&result).unwrap();
-
-    println!("{:?}", rec.result.logs[0].data);
+    match parse_cli_args()
+        .and_then(validate_cli_args)
+        .and_then(initialize_state_from_cli_args)
+        .and_then(get_endpoint_and_set_in_state)
+        .and_then(|_| Ok("Fin!"))
+        {
+            Ok(proof) => println!("{:?}", proof),
+            Err(e) => println!("{}", e)
+        }
 }
