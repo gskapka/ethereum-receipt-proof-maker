994e0d6d1aa38092f093edd7ec696d6ce8fbd8ef
Greg Kapka <greg@oraclize.it>
2019-08-01 16:59:35 +0100

    feat(state-initter): <- adds module to build state from CLI args

---
+++ src/initialize_state_from_cli_args.rs
@@ -0,0 +1,33 @@
+use crate::state::State;
+use crate::types::Result;
+use crate::parse_cli_args::CliArgs;
+use crate::utils::convert_hex_to_h256;
+
+pub fn initialize_state_from_cli_args(cli_args: CliArgs) -> Result<State> {
+    State::get_initial_state(
+        convert_hex_to_h256(cli_args.arg_txhash)?,
+        cli_args.flag_verbose
+    )
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use crate::test_utils::get_valid_dummy_hash_hex;
+
+    #[test]
+    fn should_create_state_from_cli_args() {
+        let expected_verbosity = true;
+        let tx_hash = get_valid_dummy_hash_hex();
+        let expected_tx_hash = convert_hex_to_h256(tx_hash.clone())
+            .unwrap();
+        let cli_args = CliArgs {
+            arg_txhash: tx_hash,
+            flag_verbose: expected_verbosity,
+        };
+        let state = initialize_state_from_cli_args(cli_args)
+            .unwrap();
+        assert!(state.tx_hash == expected_tx_hash);
+        assert!(state.verbose == expected_verbosity);
+    }
+}
