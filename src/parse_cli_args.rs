2cbd7a949e32f4e0b8620698bed35e38027b3770
Greg Kapka <greg@oraclize.it>
2019-08-02 19:57:35 +0100

    feat(cli-args): adds verbose mode logging to CLI arg getter

--- src/parse_cli_args.rs
+++ src/parse_cli_args.rs
@@ -2,6 +2,7 @@ use docopt::Docopt;
 use crate::types::Result;
 use crate::errors::AppError;
 use crate::usage_info::USAGE_INFO;
+use crate::utils::convert_hex_to_h256;
 
 #[derive(Debug, Deserialize)]
 pub struct CliArgs {
@@ -12,8 +13,19 @@ pub struct CliArgs {
 pub fn parse_cli_args() -> Result<CliArgs> {
     match Docopt::new(USAGE_INFO)
         .and_then(|d| d.deserialize()) {
-            Ok(cli_args) => Ok(cli_args),
-            Err(e) =>
+            Ok(cli_args) => {
+                let args: CliArgs = cli_args;
+                if args.flag_verbose {
+                    println!("\n✔ CLI Args parsed successfully!");
+                    println!("✔ Verbose mode: {}", args.flag_verbose);
+                    println!(
+                        "✔ Transaction hash: {}",
+                        convert_hex_to_h256(args.arg_txhash.clone())?
+                    );
+                }
+                Ok(args)
+            },
+            Err(_e) =>
                 Err(
                     AppError::Custom(
                         format!("✘ Error parsing CLI args!:\n{}", USAGE_INFO)
