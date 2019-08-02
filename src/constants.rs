af508217ada33980c33669812a027c32fa48fea7
Greg Kapka <greg@oraclize.it>
2019-08-02 22:04:52 +0100

    feat(constants): adds reqwest timeout to constants

--- src/constants.rs
+++ src/constants.rs
@@ -1,6 +1,7 @@
 pub const HASH_LENGTH: usize  = 32;
 pub const HASH_HEX_CHARS: usize  = 64;
 pub const HEX_PREFIX_LENGTH: usize = 2;
+pub const REQWEST_TIMEOUT_TIME: u64 = 5;
 pub const ADDRESS_LENGTH_CHARS: usize = 40;
 pub static DOT_ENV_PATH: &'static str = "./.env";
 pub static DEFAULT_ENDPOINT: &'static str = "http://localhost:8545/";
