256035587092eaaddb7ea81ea7e64c66b9cd5059
Greg Kapka <greg@oraclize.it>
2019-08-02 19:55:44 +0100

    feat(main): adds node connection to main CLI

--- src/main.rs
+++ src/main.rs
@@ -9,12 +9,14 @@ mod state;
 mod types;
 mod errors;
 mod constants;
+mod get_block;
 mod usage_info;
 mod get_config;
 mod test_utils;
 mod get_endpoint;
 mod dot_env_utils;
 mod parse_cli_args;
+mod connect_to_node;
 mod validate_tx_hash;
 mod validate_cli_args;
 mod get_rpc_call_jsons;
@@ -26,6 +28,7 @@ mod initialize_state_from_cli_args;
 
 use crate::state::State;
 use crate::parse_cli_args::parse_cli_args;
+use crate::connect_to_node::connect_to_node;
 use crate::validate_cli_args::validate_cli_args;
 use crate::get_endpoint::get_endpoint_and_set_in_state;
 use crate::initialize_state_from_cli_args::initialize_state_from_cli_args;
@@ -35,9 +38,9 @@ fn main() {
         .and_then(validate_cli_args)
         .and_then(initialize_state_from_cli_args)
         .and_then(get_endpoint_and_set_in_state)
-        .and_then(|_| Ok("Fin!"))
+        .and_then(connect_to_node)
         {
-            Ok(proof) => println!("{:?}", proof),
+            Ok(_proof) => println!("\n Fin!"),
             Err(e) => println!("{}", e)
         }
 }
