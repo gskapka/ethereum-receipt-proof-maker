b770620df73679c4b7c5c90fd6a1b4b966254fe9
Greg Kapka <greg@oraclize.it>
2019-08-09 08:43:14 +0100

    feat(state): <- adds block getter & setter & tests to that

--- src/state.rs
+++ src/state.rs
@@ -1,27 +1,37 @@
-use crate::types::Block;
 use ethereum_types::H256;
-use crate::types::Result;
 use crate::errors::AppError;
 use crate::utils::{
     get_not_in_state_err,
-    get_no_overwrite_state_err
+    get_no_overwrite_state_err,
+};
+use crate::types::{
+    Block,
+    Result,
+    Receipt,
 };
 
-#[derive(Clone)]
 pub struct State {
     pub verbose: bool,
     pub tx_hash: H256,
-    block: Option<Block>,
-    endpoint: Option<String>,
+    pub block: Option<Block>,
+    pub tx_hash_string: String,
+    pub receipt: Option<Receipt>,
+    pub endpoint: Option<String>,
 }
 
 impl State {
-    pub fn get_initial_state(tx_hash: H256, verbosity: bool) -> Result<State> {
+    pub fn get_initial_state( // TODO: Rename to init()!
+        tx_hash: H256,
+        tx_hash_string: String,
+        verbosity: bool
+    ) -> Result<State> {
         Ok(
             State {
+                tx_hash,
                 block: None,
+                receipt: None,
                 endpoint: None,
-                tx_hash: tx_hash,
+                tx_hash_string,
                 verbose: verbosity,
             }
         )
@@ -29,7 +39,8 @@ impl State {
 
     pub fn set_block_in_state(mut self, block: Block) -> Result<State> {
         match self.block {
-            Some(_) => Err(AppError::Custom(get_no_overwrite_state_err("block"))),
+            Some(_) =>
+                Err(AppError::Custom(get_no_overwrite_state_err("block"))),
             None => {
                 self.block = Some(block);
                 Ok(self)
@@ -37,9 +48,21 @@ impl State {
         }
     }
 
+    pub fn set_receipt_in_state(mut self, receipt: Receipt) -> Result<State> {
+        match self.receipt{
+            Some(_) =>
+                Err(AppError::Custom(get_no_overwrite_state_err("receipt"))),
+            None => {
+                self.receipt = Some(receipt);
+                Ok(self)
+            }
+        }
+    }
+
     pub fn set_endpoint_in_state(mut self, endpoint: String) -> Result<State> {
         match self.endpoint {
-            Some(_) => Err(AppError::Custom(get_no_overwrite_state_err("endpoint"))),
+            Some(_) =>
+                Err(AppError::Custom(get_no_overwrite_state_err("endpoint"))),
             None => {
                 self.endpoint = Some(endpoint);
                 Ok(self)
@@ -47,9 +70,9 @@ impl State {
         }
     }
 
-    pub fn get_block_from_state(self) -> Result<Block> {
-        match self.block {
-            Some(block) => Ok(block),
+    pub fn get_block_from_state(&self) -> Result<&Block> {
+        match &self.block {
+            Some(block) => Ok(&block),
             None => Err(AppError::Custom(get_not_in_state_err("block")))
         }
     }
@@ -60,6 +83,14 @@ impl State {
             None => Err(AppError::Custom(get_not_in_state_err("endpoint")))
         }
     }
+
+    // TODO: Test
+    pub fn get_receipt_from_state(&self) -> Result<&Receipt> {
+        match &self.receipt{
+            Some(receipt) => Ok(receipt),
+            None => Err(AppError::Custom(get_not_in_state_err("receipt")))
+        }
+    }
 }
 
 #[cfg(test)]
@@ -67,16 +98,18 @@ mod tests {
     use super::*;
 
     use crate::test_utils::{
-        get_valid_dummy_hash_h256,
-        get_dummy_initial_state,
+        get_expected_block,
+        get_valid_tx_hash_h256,
+        get_valid_initial_state,
+        assert_block_is_correct,
     };
 
     #[test]
     fn should_get_initial_state_correctly() {
         let expected_verbosity = true;
-        let expected_tx_hash = get_valid_dummy_hash_h256()
+        let expected_tx_hash = get_valid_tx_hash_h256()
             .unwrap();
-        let state = get_dummy_initial_state()
+        let state = get_valid_initial_state()
             .unwrap();
         assert!(state.tx_hash== expected_tx_hash);
         assert!(state.verbose == expected_verbosity);
@@ -85,9 +118,9 @@ mod tests {
     #[test]
     fn initial_state_should_have_no_block() {
         let expected_err = get_not_in_state_err("block");
-        let state = get_dummy_initial_state()
+        let state = get_valid_initial_state()
             .unwrap();
-        match State::get_block_from_state(state) {
+        match State::get_block_from_state(&state) {
             Err(AppError::Custom(e)) => assert!(e == expected_err) ,
             _ => panic!("Block should not be initialised in state!"),
         }
@@ -96,7 +129,7 @@ mod tests {
     #[test]
     fn initial_state_should_have_no_endpoint() {
         let expected_err = get_not_in_state_err("endpoint");
-        let state = get_dummy_initial_state()
+        let state = get_valid_initial_state()
             .unwrap();
         match State::get_endpoint_from_state(&state) {
             Err(AppError::Custom(e)) => assert!(e == expected_err),
@@ -106,9 +139,9 @@ mod tests {
 
     #[test]
     fn initial_state_should_have_tx_hash_set_correctly() {
-        let expected_tx_hash = get_valid_dummy_hash_h256()
+        let expected_tx_hash = get_valid_tx_hash_h256()
             .unwrap();
-        let state = get_dummy_initial_state()
+        let state = get_valid_initial_state()
             .unwrap();
         assert!(state.tx_hash == expected_tx_hash);
     }
@@ -116,29 +149,15 @@ mod tests {
     #[test]
     fn initial_state_should_have_verbosity_set_correctly() {
         let expected_verbosity = true;
-        let state = get_dummy_initial_state()
+        let state = get_valid_initial_state()
             .unwrap();
         assert!(state.verbose == expected_verbosity);
     }
 
-    /*
-    #[test]
-    fn should_add_block_to_state() { // TODO: Implement! (Need an empty block getter! | sample one! | default block struct)
-        let expected_result = "expected endpoint".to_string();
-        let state = State::get_initial_state()
-            .unwrap();
-        let new_state = State::set_endpoint_in_state(state, expected_result.clone())
-            .unwrap();
-        let result = State::get_endpoint_from_state(&new_state)
-            .unwrap();
-        assert!(result == expected_result);
-    }
-    */
-
     #[test]
     fn should_set_endpoint_to_state() {
         let expected_result = "expected endpoint".to_string();
-        let state = get_dummy_initial_state()
+        let state = get_valid_initial_state()
             .unwrap();
         let new_state = State::set_endpoint_in_state(state, expected_result.clone())
             .unwrap();
@@ -151,7 +170,7 @@ mod tests {
     fn should_err_when_attempting_to_overwrite_endpoint_in_state() {
         let expected_err = "✘ Cannot overwrite endpoint in state!";
         let dummy_endpoint = "dummy endpoint".to_string();
-        let initial_state = get_dummy_initial_state()
+        let initial_state = get_valid_initial_state()
             .unwrap();
         let state_with_endpoint = State::set_endpoint_in_state(
             initial_state,
@@ -166,6 +185,38 @@ mod tests {
         match State::set_endpoint_in_state(
             state_with_endpoint,
             dummy_endpoint.clone()
+        ) {
+            Err(AppError::Custom(e)) => assert!(e == expected_err),
+            _ => panic!("Overwriting state should not have succeeded!"),
+        }
+    }
+
+    #[test]
+    fn should_set_block_in_state() {
+        let expected_block = get_expected_block();
+        let state = get_valid_initial_state()
+            .unwrap();
+        let new_state = State::set_block_in_state(state, expected_block)
+            .unwrap();
+        let result = State::get_block_from_state(&new_state)
+            .unwrap();
+        assert_block_is_correct(result.clone())
+    }
+
+    #[test]
+    fn should_err_when_attempting_to_overwrite_block_in_state() {
+        let expected_err = "✘ Cannot overwrite block in state!";
+        let expected_block = get_expected_block();
+        let state = get_valid_initial_state()
+            .unwrap();
+        let state_with_block = State::set_block_in_state(
+            state,
+            expected_block.clone()
+        )
+            .unwrap();
+        match State::set_block_in_state(
+            state_with_block,
+            expected_block
         ) {
             Err(AppError::Custom(e)) => assert!(e == expected_err),
             _ => panic!("Overwriting state should not have succeeded!"),
