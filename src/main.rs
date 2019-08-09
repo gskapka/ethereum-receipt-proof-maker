b3217790036e06744dacccfc7a638435259d1f21
Greg Kapka <greg@oraclize.it>
2019-08-09 11:31:22 +0100

    feat(main): adds more to main pipeline

--- src/main.rs
+++ src/main.rs
@@ -8,13 +8,15 @@ mod utils;
 mod state;
 mod types;
 mod errors;
+mod get_log;
 mod constants;
 mod get_block;
 mod usage_info;
 mod get_config;
 mod test_utils;
+mod get_receipt;
 mod get_endpoint;
-mod dot_env_utils;
+mod make_rpc_call;
 mod parse_cli_args;
 mod connect_to_node;
 mod validate_tx_hash;
@@ -24,12 +26,14 @@ mod initialize_state_from_cli_args;
 
 #[macro_use] extern crate serde_json;
 #[macro_use] extern crate serde_derive;
-#[macro_use] extern crate serial_test_derive;
+#[cfg(test)] #[macro_use] extern crate serial_test_derive;
 
+use crate::state::State;
 use crate::parse_cli_args::parse_cli_args;
 use crate::connect_to_node::connect_to_node;
 use crate::validate_cli_args::validate_cli_args;
 use crate::get_endpoint::get_endpoint_and_set_in_state;
+use crate::get_block::get_block_from_tx_hash_in_state_and_add_to_state;
 use crate::initialize_state_from_cli_args::initialize_state_from_cli_args;
 
 fn main() {
@@ -38,8 +42,8 @@ fn main() {
         .and_then(initialize_state_from_cli_args)
         .and_then(get_endpoint_and_set_in_state)
         .and_then(connect_to_node)
-        {
-            Ok(_proof) => println!("\n Fin!"),
+        .and_then(get_block_from_tx_hash_in_state_and_add_to_state) {
+            Ok(_proof) => println!("\n✔ Fin!"),
             Err(e) => println!("{}", e)
         }
 }
