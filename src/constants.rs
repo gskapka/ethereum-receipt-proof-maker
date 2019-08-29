8bb9636a4db4ef5ddb58b064932837fb8cdf5734
Greg Kapka <greg@oraclize.it>
2019-08-29 18:17:59 +0100

    feat(constants): <- adds a hashed null node to that

--- src/constants.rs
+++ src/constants.rs
@@ -1,4 +1,5 @@
 use crate::types::Byte;
+use ethereum_types::H256;
 use crate::nibble_utils::Nibbles;
 
 pub const ZERO_BYTE: u8 = 0u8;
@@ -11,6 +12,13 @@ pub const NUM_NIBBLES_IN_BYTE: usize = 2;
 pub const HIGH_NIBBLE_MASK: Byte = 15u8; // NOTE: 15u8 == [0,0,0,0,1,1,1,1]
 pub const ADDRESS_LENGTH_CHARS: usize = 40;
 pub static DOT_ENV_PATH: &'static str = "./.env";
-pub static EMPTY_NODE: crate::types::Node<'static> = (&[], None);
+pub const HASHED_NULL_NODE: H256 = H256(HASHED_NULL_NODE_BYTES);
 pub static DEFAULT_ENDPOINT: &'static str = "http://localhost:8545/";
 pub const EMPTY_NIBBLES: Nibbles = Nibbles { data: Vec::new(), offset: 0 };
+
+const HASHED_NULL_NODE_BYTES: [u8; 32] = [ // NOTE: keccak hash of the RLP of null
+    0x56, 0xe8, 0x1f, 0x17, 0x1b, 0xcc, 0x55, 0xa6,
+    0xff, 0x83, 0x45, 0xe6, 0x92, 0xc0, 0xf8, 0x6e,
+    0x5b, 0x48, 0xe0, 0x1b, 0x99, 0x6c, 0xad, 0xc0,
+    0x01, 0x62, 0x2f, 0xb5, 0xe3, 0x63, 0xb4, 0x21
+];
